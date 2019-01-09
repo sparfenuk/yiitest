5ac6779f3abd9c80fde42c3d26e8bd76fd94a84b
pacman_n <sparfenuk13@gmail.com>
2019-01-09 16:58:21 +0200

    ssss

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -185,29 +185,13 @@ $this->registerJsFile('https://oss.maxcdn.com/respond/1.4.2/respond.min.js');
     <!-- container -->
 </header>
 <!--/header-->
+
 <!--content-->
 
-<div class="goods">
-    <?php
-
-    foreach ($dataProvider->models as $goods) {
-        echo '<div class="row">';
-
-        //  if ($post->image_id === 0) {
-        echo '<div class="image col-md-3"><img style="max-width: 50%" src="' . Yii::$app->params['basePath'] . '/images/No_image_3x4.svg.png" > </div>';
-        // }
-
-        echo '<div class="col-md-9">' . '<div class="name">'. '<a href="goodsPage.php">' .HTML::encode($goods->name) .'</a>'.'</div>'. '</div>';
-
-        echo '<div class="col-md-9">' . '<div class="price">' . HTML::encode($goods->price) . '</div>'.'</div>';
-
-        echo '</div>';
-    }
-    //    echo '</pre>';
-    ?>
-</div>
+<?//= $content ?>
 
 <!--/content-->
+
 <!-- FOOTER -->
 <footer id="footer" class="section section-grey">
     <!-- container -->
