fa51f454c13b052e0c59a71b77b5abc937fcc9ce
developer <sparfenuk13@gmail.com>
2019-10-03 13:03:58 +0300

    configs

--- config/params.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'adminEmail' => 'sparfenuk13@gmail.com',
-    'mailEmail' => 'e.shop.yii2@gmail.com',
-    'SALT'=>'$2y$10$hpqEFEvc1rnw2xd/PqRmTOXsssGYexRFMZosEOGoWzt58XpMsXOK8h0S4Mi',
-    'webPath' => "D:\OpenServer\OSPanel\domains\yiitest\web\\",
-];
